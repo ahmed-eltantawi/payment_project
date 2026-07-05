5afe862a7ae8f316aae03a540662d6849ee3385d
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-07-05 08:19:47 +0300

    stripePublishableKey

--- lib/core/utils/constants.example.dart
+++ lib/core/utils/constants.example.dart
@@ -2,4 +2,5 @@
 abstract class Constants {
   static const String apiKey = "YOUR_API_KEY";
   static const String cardPaymentMethodIntegrationId = "1234567";
+  static const String stripePublishableKey = "your stripe publishable key";
 }
