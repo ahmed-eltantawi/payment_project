178c169fe3c058bf6b29944b0f1f75bd7801957e
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-07-08 01:49:37 +0300

    add paymobMobileWalletId var

--- lib/core/utils/constants.example.dart
+++ lib/core/utils/constants.example.dart
@@ -4,6 +4,7 @@ abstract class Constants {
   static const String paymobSecretKey = "YOUR PAYMOB SECRET KEY";
   static const String paymobPublicKey = "YOUR PAYMOB PUBLIC KEY";
   static const int paymobOnlineCardId = 012345678;
+  static const int paymobMobileWalletId = 12345678;
 
   static const String stripePublishableKey = "your stripe publishable key";
   static const String stripeSecretKey = "stripeSecretKey";
